38c74b797290e36c98c1af921ec11ffc79060442
zherdiev <zherdiev@ununtuForAK2.myguest.virtualbox.org>
2023-12-11 21:16:09 +0200

    change hello.c

--- hello.c
+++ hello.c
@@ -32,20 +32,61 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/printk.h>
+#include <linux/list.h>
+#include <linux/ktime.h>
+#include <linux/stab.h>
 
-MODULE_AUTHOR("Serhii Popovych <serhii.popovych@globallogic.com>");
+MODULE_AUTHOR("Zherdiev Danylo <zerdevdanil430@gmail.com>");
 MODULE_DESCRIPTION("Hello, world in Linux Kernel Training");
 MODULE_LICENSE("Dual BSD/GPL");
+struct my_data {
+	struct list_head list;
+	ktime_t time;
+};
+
+
+static unsigned int number_of_iterations = 1;
+module_param(number_of_iterations, uint, S_IRUGO);
+MODULE_PARM_DESC(number_of_iterations, "Number of how many times 'Hello world' will be printed");
+
+static LIST_HEAD(my_list_head);
 
 static int __init hello_init(void)
 {
-	printk(KERN_EMERG "Hello, world!\n");
+	if (number_of_iterations == 0) {
+		printk(KERN_WARNING "You printed 0 iterations");
+	} else if (number_of_iterations >= 5 && number_of_iterations <= 10) {
+		printk(KERN_WARNING "You printed between 5 and 10");
+	} else if (number_of_iterations > 10) {
+		return -EINVAL;
+	}
+
+	int counter = 0;
+	while (number_of_iterations != counter) {
+		struct my_data *my_data = kmalloc(sizeof(*my_data), GFP_KERNEL);
+
+		my_data->time = ktime_get();
+		list_add(&my_data->list, &my_list_head);
+		printk(KERN_INFO "Hello, world!\n");
+		counter++;
+
+	}
+
 	return 0;
 }
 
 static void __exit hello_exit(void)
 {
-	/* Do nothing here right now */
+	struct my_data *my_data,  *next;
+	list_for_each_entry_safe(my_data, next, &my_list_head, list) {
+		printk(KERN_INFO "Time: %lld nano seconds\n", ktime_to_ns(my_data->time));
+
+		list_del(&my_data->list);
+
+		kfree(my_data);
+
+	}
+
 }
 
 module_init(hello_init);
